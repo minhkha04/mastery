3876c325f93032b2d0b19f91302b046a54dc946d
MinhKha <minhkha.04.contact@gmail.com>
2024-10-24 16:49:51 +0700

    hide subnav in mobe

--- acess/javascript/index.js
+++ acess/javascript/index.js
@@ -1,8 +1,11 @@
 const btnSub = document.getElementById("add");
 const open1 = document.getElementById("open1");
-
+const list = document.querySelectorAll('#move');
 function toggleMenu() {
-    open1.classList.toggle('open');  // Toggle hiển thị/ẩn lớp 'open'
-}
+    open1.classList.toggle('open');
+}   
 
 btnSub.addEventListener('click', toggleMenu);
+for(let e of list) {
+    e.addEventListener('click', toggleMenu);
+}
